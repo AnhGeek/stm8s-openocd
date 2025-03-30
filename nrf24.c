034630fd27d0164d814a2c4d7ec2b35064c4334b
AnhGeek <lyhoanganh.hcm05@gmail.com>
2025-03-30 14:30:02 -0400

    add read, write address - add fnc send payload - not work yet

---
+++ nrf24.c
@@ -0,0 +1,183 @@
+#include <stdint.h>
+#include "spi.h"
+#include "nrf24.h"
+
+# define __STATIC_INLINE
+// Send command to NRF24L01 module
+uint8_t nrf_write_cmd(uint8_t cmd) 
+{
+    CSN_LOW();
+    uint8_t s = SPI_write(cmd);
+    CSN_HIGH();
+    return s;
+}
+
+// Read NRF24L01's register
+uint8_t nrf_read_register(uint8_t regNo) 
+{
+    CSN_LOW();
+    SPI_write(NRF24_R_REGISTER | (NRF24_REGISTER_MASK & regNo));
+    uint8_t r = SPI_write(NRF24_NOP);
+    CSN_HIGH();
+    return r;
+}
+
+// Write a value to NRF24L01's register
+uint8_t __STATIC_INLINE nrf_write_register(uint8_t regNo, uint8_t regVal) {
+    CSN_LOW();
+    uint8_t s = SPI_write(NRF24_W_REGISTER | (NRF24_REGISTER_MASK & regNo));
+    SPI_write(regVal);
+    CSN_HIGH();
+    return s;
+}
+
+void __STATIC_INLINE nrf_write_addr(uint8_t a, const uint8_t* data, uint8_t len)
+{
+    CSN_LOW();
+    SPI_write(NRF24_W_REGISTER | (NRF24_REGISTER_MASK & a));
+    while (len--) {
+        SPI_write(*data++);
+    }
+    CSN_HIGH();
+}
+
+// Read NRF24L01's register
+uint8_t __STATIC_INLINE nrf_read_addr(uint8_t regNo, uint8_t len)
+{
+    uint8_t r;
+    CSN_LOW();
+    SPI_write(NRF24_R_REGISTER | (NRF24_REGISTER_MASK & regNo));
+    while (len--) {
+        r = SPI_write(NRF24_NOP);
+    }
+    CSN_HIGH();
+    return r;
+}
+
+uint8_t nrf_detect(void) 
+{  
+    nrf_write_cmd(NRF24_FLUSH_TX);
+    uint8_t fifo_status;
+    uint8_t returnCode = NRF24L01_DETECTED;
+    fifo_status = nrf_read_register(NRF24_FIFO_STATUS_REG);
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_EMPTY) != NRF24_FIFO_STATUS_TX_EMPTY) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_FULL) == NRF24_FIFO_STATUS_TX_FULL) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+  
+    // send dummy 1-byte payload to the 1st FIFO buffer
+    CSN_LOW();
+    SPI_write(NRF24_W_TX_PAYLOAD);
+    SPI_write(0);
+    CSN_HIGH();
+    
+    fifo_status = nrf_read_register(NRF24_FIFO_STATUS_REG);
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_EMPTY) == NRF24_FIFO_STATUS_TX_EMPTY) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+  
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_FULL) == NRF24_FIFO_STATUS_TX_FULL) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+    
+    // send dummy 1-byte payload to the 2nd FIFO buffer
+    CSN_LOW();
+    SPI_write(NRF24_W_TX_PAYLOAD);
+    SPI_write(0);
+    CSN_HIGH();
+    
+    fifo_status = nrf_read_register(NRF24_FIFO_STATUS_REG);
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_EMPTY) == NRF24_FIFO_STATUS_TX_EMPTY) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+  
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_FULL) == NRF24_FIFO_STATUS_TX_FULL) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+  
+    // send dummy 1-byte payload to the 3rd FIFO buffer
+    CSN_LOW();
+    SPI_write(NRF24_W_TX_PAYLOAD);
+    SPI_write(0);
+    CSN_HIGH();
+  
+    fifo_status = nrf_read_register(NRF24_FIFO_STATUS_REG);
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_EMPTY) == NRF24_FIFO_STATUS_TX_EMPTY) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+  
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_FULL) != NRF24_FIFO_STATUS_TX_FULL) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+  
+    nrf_write_cmd(NRF24_FLUSH_TX);
+    
+    fifo_status = nrf_read_register(NRF24_FIFO_STATUS_REG);
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_EMPTY) != NRF24_FIFO_STATUS_TX_EMPTY) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+  
+    if ((fifo_status & NRF24_FIFO_STATUS_TX_FULL) == NRF24_FIFO_STATUS_TX_FULL) {
+        returnCode = NRF24L01_NOT_DETECTED;
+    }
+    
+    return returnCode;
+}  
+
+
+uint8_t __STATIC_INLINE nrf_init(uint8_t channel) {
+    nrf_write_register(NRF24_SETUP_RETR, 0x5F);  // CONFIG: ARD=5, ARC=15
+    nrf_write_register(NRF24_RF_SETUP_REG, 0x06); //'00' – 1Mbps, '11' – 0dBm, '0'
+    nrf_write_register(NRF24_DYNPD_REG, 0x00); // disable dynamic payloads by default (for all pipes)
+    nrf_write_register(NRF24_EN_AA_REG, 0x3F);  // enable auto-ack on all pipes
+    nrf_write_register(NRF24_EN_EN_RXADDR, 3); // only open RX pipes 0 & 1
+    nrf_write_register(NRF24_RX_PW_P0, 32);     // set static payload size to 32 (max) bytes by default
+    nrf_write_register(NRF24_SETUP_AW_REG, 3);  // set default address length to (max) 5 bytes
+    
+    // Set radio channel number
+    nrf_write_register(NRF24_RF_CH_REG, channel);
+
+    // Reset current status
+    // Notice reset and flush is the last thing we do
+    nrf_write_register(NRF24_STATUS_REG, NRF24_CONFIG_RX_DR | NRF24_CONFIG_TX_DS | NRF24_CONFIG_MAX_RT);
+
+    nrf_write_register(NRF24_CONFIG_REG, 0x0E);  // CONFIG: Enable CRC, TX mode
+    
+    return 1;
+}
+
+uint8_t __STATIC_INLINE nrf_openWritingPipe(const uint8_t* address)
+{
+    // Note that AVR 8-bit uC's store this LSB first, and the NRF24L01(+)
+    // expects it LSB first too, so we're good.
+    nrf_write_addr(NRF24_RX_ADDR_P0_REG, address, 5);
+    nrf_write_addr(NRF24_TX_ADDR_REG, address, 5);
+}
+
+void nrf_sendpayload(uint8_t *data, uint8_t len) {
+    uint16_t ms = 1;
+    uint16_t nrf_status;
+    CSN_LOW();
+    SPI_write(NRF24_W_TX_PAYLOAD);
+    for (uint8_t i = 0; i < len; i++) {
+        SPI_write(data[i]);
+    }
+    CSN_HIGH();
+
+    // Start transmission
+    CE_HIGH();
+    while (ms--) {
+        for (uint16_t i = 0; i < 1600; i++) __asm__("nop");
+    }
+    CE_LOW();
+    
+    nrf_status = nrf_read_register(NRF24_STATUS_REG);
+    if(nrf_status & NRF24_CONFIG_MAX_RT)
+    {
+        //flush_tx
+        nrf_read_register(NRF24_FLUSH_TX); 
+    }
+    nrf_write_register(NRF24_STATUS_REG, NRF24_CONFIG_RX_DR | NRF24_CONFIG_TX_DS | NRF24_CONFIG_MAX_RT);
+}
---
+++ nrf24.h
@@ -0,0 +1,77 @@
+#ifndef __NRF24_H__
+#define __NRF24_H__
+#include <stdint.h>
+#include "spi.h"
+
+#define NRF24L01_DETECTED       1
+#define NRF24L01_NOT_DETECTED   0
+
+#define NRF24_CONFIG_REG        0x00
+#define NRF24_EN_AA_REG         0x01
+#define NRF24_EN_EN_RXADDR      0x02
+#define NRF24_SETUP_AW_REG      0x03
+#define NRF24_SETUP_RETR        0x04
+#define NRF24_RF_CH_REG         0x05
+#define NRF24_RF_SETUP_REG      0x06
+#define NRF24_STATUS_REG        0x07
+#define NRF24_RX_ADDR_P0_REG    0x0A
+#define NRF24_TX_ADDR_REG       0x10
+#define NRF24_RX_PW_P0          0x11
+#define NRF24_FIFO_STATUS_REG   0x17
+#define NRF24_DYNPD_REG         0x1C
+#define NRF24_FEATURE_REG       0x1D 
+          
+// 00: CONFIG register bits from 0 t_die 6, bit 7 must be set to 0
+// ============================================================
+#define NRF24_CONFIG_PRIM_RX    (1 << 0)
+#define NRF24_CONFIG_PWR_UP     (1 << 1)
+#define NRF24_CONFIG_CRCO       (1 << 2)
+#define NRF24_CONFIG_EN_CRC     (1 << 3)
+#define NRF24_CONFIG_MAX_RT     (1 << 4)
+#define NRF24_CONFIG_TX_DS      (1 << 5)
+#define NRF24_CONFIG_RX_DR      (1 << 6)
+
+// 07: STATUS -- Status Register (In parallel to the SPI command
+//               word applied on the MOSI pin, the STATUS register
+//               is shifted serially out on the MISO pin)   (bits from 0 to 6)
+// ===============================================================
+#define NRF24_STATUS_MAX_RT     NRF24_CONFIG_MAX_RT
+#define NRF24_STATUS_TX_DS      NRF24_CONFIG_TX_DS
+#define NRF24_STATUS_RX_DR      NRF24_CONFIG_RX_DR
+
+// 06: RF_SETUP -- RF Setup Register (bits from 0 to 6)
+// ====================================================
+#define NRF24_RF_SETUP_RF_PWR_0         (1 << 0)
+#define NRF24_RF_SETUP_RF_PWR_1         (1 << 1)
+#define NRF24_RF_SETUP_RF_DR_HIGH       (1 << 3)
+#define NRF24_RF_SETUP_RF_DR_LOW        (1 << 5)
+          
+#define NRF24_FIFO_STATUS_RX_EMPTY      (1 << 0)
+#define NRF24_FIFO_STATUS_TX_FULL       (1 << 5)
+#define NRF24_FIFO_STATUS_TX_EMPTY      (1 << 4)
+        
+#define NRF24_FEATURE_EN_DYN_ACK        (1 << 0)
+#define NRF24_FEATURE_EN_DPL            (1 << 2)
+
+          
+#define NRF24_R_REGISTER        0x00
+#define NRF24_W_REGISTER        0x20
+#define NRF24_REGISTER_MASK     0x1F
+#define NRF24_R_RX_PL_WID       0x60
+#define NRF24_R_RX_PAYLOAD      0x61
+#define NRF24_W_TX_PAYLOAD      0xA0
+
+#define NRF24_FLUSH_RX          0xE2
+#define NRF24_FLUSH_TX          0xE1
+
+#define NRF24_NOP               0xFF       
+
+extern uint8_t nrf_write_cmd(uint8_t cmd);
+extern uint8_t nrf_detect(void);
+uint8_t __STATIC_INLINE nrf_init(uint8_t channel);
+void nrf_sendpayload(uint8_t *data, uint8_t len);
+uint8_t __STATIC_INLINE nrf_read_register(uint8_t regNo);
+void __STATIC_INLINE nrf_write_addr(uint8_t a, const uint8_t* data, uint8_t len);
+uint8_t __STATIC_INLINE nrf_read_addr(uint8_t regNo, uint8_t len);
+uint8_t __STATIC_INLINE nrf_openWritingPipe(const uint8_t* address);
+#endif
