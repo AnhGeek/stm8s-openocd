0043ea7dc162bfc9b6ea9f6d594478f08c130f44
AnhGeek <lyhoanganh.hcm05@gmail.com>
2025-03-30 14:29:06 -0400

    add CE function

--- spi.c
+++ spi.c
@@ -9,28 +9,32 @@
  */
 
 void SPI_init() {
-    /* Initialize CS pin */
+    /* Initialize CS pin, default High */
     PC_DDR |= (1 << CS_PIN);
     PC_CR1 |= (1 << CS_PIN);
     PC_ODR |= (1 << CS_PIN);
 
-    // CE pin, default is low
+    // CE pin, default is Low
     PC_DDR |= (1 << CE_PIN);
     PC_CR1 |= (1 << CE_PIN);
-    PC_ODR |= (1 << CE_PIN);
+    PC_ODR &= ~(1 << CE_PIN);
 
     /* Initialize SPI master at 500kHz  */
+    /* Set master mode */ 
     SPI_CR2 = (1 << SSM) | (1 << SSI);
     SPI_CR1 = (1 << MSTR) | (1 << SPE) | (1 << BR0);
 }
 
-void SPI_write(uint8_t data) {
-    SPI_DR = data;
+// Bidirectional SPI transfer function
+uint8_t SPI_write(uint8_t data) {
     while (!(SPI_SR & (1 << TXE)));
+    SPI_DR = data;
+    while (!(SPI_SR & (1 << RXNE)));
+    return SPI_DR;
 }
 
-uint8_t SPI_read() {
-    SPI_write(0xFF);
+uint8_t SPI_read(uint8_t data) {
+    SPI_write(data);
     while (!(SPI_SR & (1 << RXNE)));
     return SPI_DR;
 }
@@ -40,5 +44,16 @@ void chip_select() {
 }
 
 void chip_deselect() {
+    while ((SPI_SR & (1 << BSY)));
     PC_ODR |= (1 << CS_PIN);
 }
+
+void ce_low() {
+    PC_ODR &= ~(1 << CE_PIN);
+}
+
+void ce_high() {
+    PC_ODR |= (1 << CE_PIN);
+}
+
+
--- spi.h
+++ spi.h
@@ -1,3 +1,5 @@
+#ifndef __SPI_H__
+#define __SPI_H__
 #include <stdint.h>
 #include "register.h"
 
@@ -14,9 +16,20 @@
 #define CS_PIN      3 // PC3
 #define CE_PIN      4 // PC4
 
+#define CSN_LOW()   chip_select()
+#define CSN_HIGH()   chip_deselect()
+
+// Pin Mapping
+#define CE_HIGH()   ce_high()
+#define CE_LOW()    ce_low()
+
 
 void SPI_init();
-void SPI_write(uint8_t data);
-uint8_t SPI_read();
+uint8_t SPI_write(uint8_t data);
+uint8_t SPI_read(uint8_t data);
 void chip_select();
 void chip_deselect();
+void ce_low();
+void ce_high();
+
+#endif
